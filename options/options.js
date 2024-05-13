6b4c76c96459626632d96ea56aa631b5a925f4af
Crowdbotics <engineering@crowdbotics.com>
2024-05-13 05:32:07 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "samk-352",
+  url: "https://samk-352.botics.co",
+  api: "https://samk-352.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
